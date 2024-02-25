d1d7196f12fc9e394e421b7ae0003f86633d1ff5
improdutividade <156726836+improdutividade@users.noreply.github.com>
2024-02-24 22:10:05 -0300

    Update appartia.py

--- appartia.py
+++ appartia.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import datetime
-import os
 import base64
-from io import BytesIO
 import uuid
 
 st.set_page_config(
@@ -39,22 +37,11 @@ class RegistroAtividades:
 
     def registrar_atividades(self):
         self.obter_informacoes_iniciais()
-
-        # Registrar atividade para o usuário atual
         self.registrar_atividade()
-
-        # Adiciona botão para download do Excel preenchido
-        if st.button("Baixar Relatório Excel"):
-            self.gerar_relatorio_excel()
-
-        # Adiciona botão para reiniciar os dados
-        if st.button("Zerar Dados"):
-            self.zerar_dados()
+        self.gerar_relatorio_excel()
 
     def registrar_atividade(self):
         st.write(f"Atividade para o usuário {st.session_state.registro['nome_usuario']}:")
-
-        # Alterado de solicitar função para informar atividade em andamento
         atividade_em_andamento = st.text_input("Informe a atividade em andamento: ").upper()
 
         iniciar = st.button("Iniciar atividade")
@@ -118,11 +105,6 @@ class RegistroAtividades:
         else:
             st.warning("Nenhum dado disponível para exportação.")
 
-    def zerar_dados(self):
-        st.write("Zerando dados...")
-        st.session_state.registro['df'] = pd.DataFrame(columns=['ID', 'Nome_Usuário', 'Numero_Projeto', 'Atividade', 'Data', 'Início', 'Fim', 'Duração'])
-        st.success("Dados zerados. Você pode iniciar novos registros.")
-
 # Função auxiliar para criar botão de download
 def get_binary_file_downloader_html(bin_file, file_label='File'):
     with open(bin_file, 'rb') as f:
@@ -135,16 +117,5 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
 user_id = str(uuid.uuid4())
 registro = RegistroAtividades(user_id)
 
-# Função principal
-def main():
-    st.sidebar.title("Menu de Navegação")
-    app_choice = st.sidebar.radio("Selecione uma opção:", ("App 1 - AtividadeTracker", "Informações", "Gráficos"))
-
-    if app_choice == "App 1 - AtividadeTracker":
-        registro.registrar_atividades()
-
-    elif app_choice == "Informações":
-        informacoes()
-
-    elif app_choice == "Gráficos":
-        graficos()
+# Executa o aplicativo
+registro.registrar_atividades()
